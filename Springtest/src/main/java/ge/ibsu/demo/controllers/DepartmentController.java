4d2ba4c598ef5377c6e16f2cc27d76df17ea8efc
nikadarejanashvili <darejanashvilinika001@gmail.com>
2026-03-31 16:41:38 +0400

    Add files via upload

---
+++ Springtest/src/main/java/ge/ibsu/demo/controllers/DepartmentController.java
@@ -0,0 +1,31 @@
+package ge.ibsu.demo.controllers;
+
+import ge.ibsu.demo.entities.Department;
+import ge.ibsu.demo.services.DepartmentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/departments")
+public class DepartmentController {
+
+   private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping("/all")
+    public List<Department> getAll() {
+        return departmentService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Department getById(@PathVariable Long id) throws Exception {
+        return departmentService.getById(id);
+    }
+}
---
+++ Springtest/src/main/java/ge/ibsu/demo/controllers/EmployeeController.java
@@ -0,0 +1,31 @@
+package ge.ibsu.demo.controllers;
+
+import ge.ibsu.demo.entities.Employee;
+import ge.ibsu.demo.services.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/all")
+    public List<Employee> getAll() {
+        return employeeService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getById(@PathVariable Long id) throws Exception {
+        return employeeService.getById(id);
+    }
+}
