29a37a38296800ff794a786e5b4ea08ca497ec80
Nick736 <darejanashvilinika00@gmail.com>
2026-03-31 17:05:23 +0400

    Test Changes

--- ibsu-java-geo-class-2026-main/ibsu-java-geo-class-2026-main/src/main/java/ge/ibsu/demo/DemoApplication.java
+++ ibsu-java-geo-class-2026-main/ibsu-java-geo-class-2026-main/src/main/java/ge/ibsu/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package ge.ibsu.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//git add .
+//git commit -m "Changes Added"
+//git push origin main
 @SpringBootApplication
 public class DemoApplication {
 
