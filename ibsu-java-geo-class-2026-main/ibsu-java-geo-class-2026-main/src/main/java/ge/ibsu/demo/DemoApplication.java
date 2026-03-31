63b458343bd83cd3a3c3604083edd8df4bae60b3
Nick736 <darejanashvilinika00@gmail.com>
2026-03-31 17:38:58 +0400

    Final Changes

--- ibsu-java-geo-class-2026-main/ibsu-java-geo-class-2026-main/src/main/java/ge/ibsu/demo/DemoApplication.java
+++ ibsu-java-geo-class-2026-main/ibsu-java-geo-class-2026-main/src/main/java/ge/ibsu/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //git add .
 //git commit -m "Changes Added"
 //git push origin main/git pull --rebase origin main
+//git clone https://github.com/Nick-736/FirstSpringApp.git
 @SpringBootApplication
 public class DemoApplication {
 
